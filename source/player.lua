a636d9a997a2497e860f5a4b26955d9e385db14d
chickenharm <121137279+chickenharm@users.noreply.github.com>
2026-09-15 08:53:30 -0400

    Revert "added fall state"
    This reverts commit 16db04e776ef7551c5fefa8adc099ff761c34649.

--- source/player.lua
+++ source/player.lua
@@ -168,10 +168,6 @@ function Player:changeToJumpState()
     self:changeState("jump")
 end
 
-function Player:changeToFallState()
-    self:changeState("fall")
-end
-
 function Player:checkForConsumeJump()
     if self.jumpBufferTimer > 0 and (self.grounded or self.coyoteTimer > 0) then
         self.yVelocity = JUMP_VELOCITY
